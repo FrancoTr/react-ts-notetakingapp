f1a23a1ffa92a5416c3e1b50b42112163b9b6a2c
Franco Traverso <franco-t20@outlook.com>
2022-12-02 22:36:02 -0300

    Add routes to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<h1>Hi!</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
