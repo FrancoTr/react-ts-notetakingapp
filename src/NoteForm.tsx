2007e9dbed43f2697e729f53ca5eca3b6a49fe3a
Franco Traverso <franco-t20@outlook.com>
2022-12-06 22:35:32 -0300

    Add multiselect

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,5 @@
 import { Row, Col, Form, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
 
 export function NoteForm() {
   return (
@@ -9,6 +10,12 @@ export function NoteForm() {
             <Form.Group controlId='title'>
               <Form.Label>Title</Form.Label>
               <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId='tags'>
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
             </Form.Group>
           </Col>
         </Row>
