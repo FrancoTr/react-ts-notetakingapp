05744b41f08d92c076cff796b96dec043d1f61c8
Franco Traverso <franco-t20@outlook.com>
2022-12-21 21:16:52 -0300

    Add notelist component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { NoteList } from "./NoteList";
 
 export type Note = {
   id: string;
@@ -54,7 +55,7 @@ function App() {
   return (
     <Container className='my-4'>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/' element={<NoteList />} />
         <Route
           path='/new'
           element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />}
